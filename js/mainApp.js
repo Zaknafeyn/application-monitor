250ee615d397caebd5988b0adec27583d6e8bb9b
sudodoki <smd.deluzion@gmail.com>
2014-08-28 00:52:14 +0300

    Add export to GDrive: http://youtu.be/uvA1T_FRtPY. TODO: add email notifications to kottans at firebase update

--- js/mainApp.js
+++ js/mainApp.js
@@ -6,6 +6,22 @@
     var kottansRef = new Firebase('https://sudodoki.firebaseio.com/kottans/'),
       auth = $firebaseSimpleLogin(kottansRef);
 
+    $scope.filterExpr = '';
+    $scope.filterCourses = [];
+    $scope.filterCourseCompose = 'OR';
+    $scope.courseFilterFunction = function (kottan) {
+      var filterCourses = $scope.filterCourses.filter(angular.identity);
+      if (filterCourses.length === 0) { return true }
+      var method = ($scope.filterCourseCompose === 'AND') ? 'every' : 'some';
+      var input, result;
+      input = (angular.isArray(kottan.courses)) ? kottan.courses : [kottan.courses];
+      result = input[method](function (course) {
+        var result = ($scope.filterCourseCompose === 'AND') ? filterCourses.length === input.length : true;
+        return result && filterCourses.indexOf(course) > -1;
+      });
+      return result;
+    }
+
     $scope.auth = auth;
     $scope.auth.$getCurrentUser().then(function(user){
       if (user) {
@@ -14,13 +30,94 @@
     });
 
     $scope.login = function(){
-      auth.$login('google', {
-        rememberMe: true
+      return auth.$login('google', {
+        rememberMe: true,
+        scope: 'https://spreadsheets.google.com/feeds https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
       }).then(function(){
         loadKottans();
       });
     };
     $scope.logout = auth.$logout;
+
+    function getCsvData(kottans) {
+      var CSV = [
+        '"name", "email", "skype", "js", "java", "ruby"'
+      ];
+      function encode_utf8(s) {
+        return unescape( encodeURIComponent( s ) );
+      }
+      kottans.forEach(function(kottan) {
+        var res = '';
+        var courses = angular.isArray(kottan.courses) ? kottan.courses : [kottan.courses]
+        res += '"' + encode_utf8(kottan.name)  + '", ';
+        res += '"' + kottan.email + '", ';
+        res += '"' + kottan.skype + '", ';
+        res += '"' + (courses.indexOf('js') > -1 ? 'TRUE' : 'FALSE') + '", '
+        res += '"' + (courses.indexOf('java') > -1 ? 'TRUE' : 'FALSE') + '", '
+        res += '"' + (courses.indexOf('ruby') > -1 ? 'TRUE' : 'FALSE') + '"'
+        CSV.push(res)
+      })
+      CSV = CSV.join('\n');
+      // returning base64encoded data
+      return btoa(CSV);
+    }
+    function formAjaxData(csvData) {
+      var metadata = {
+        title: 'kottans_export.csv',
+        mimeType: 'application/vnd.google-apps.spreadsheet',
+      }
+      // mimeType: "application/vnd.google-apps.spreadsheet"
+      var boundary = '-------314159265358979323846';
+      var delimiter = "\r\n--" + boundary + "\r\n";
+      var close_delim = "\r\n--" + boundary + "--";
+      var multipartRequestBody =
+        delimiter +
+        'Content-Type: application/json\r\n\r\n' +
+        JSON.stringify(metadata) +
+        delimiter +
+        'Content-Type: text/csv' + '\r\n' +
+        'Content-Transfer-Encoding: base64\r\n' +
+        '\r\n' +
+        csvData +
+        close_delim;
+      return [multipartRequestBody, boundary];
+    }
+    function doPost(ajaxData, boundary) {
+      return $.ajax({
+        type: "POST",
+        headers: {
+            Authorization: "Bearer " + $scope.auth.user.accessToken
+        },
+        url: "https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart",
+        data: ajaxData,
+        processData: false,
+        contentType: 'multipart/mixed;boundary=' + boundary
+      }).done(function(response) {
+          $scope.lastExportLink = response.alternateLink;
+          $scope.$apply();
+          console.log("post succeeded");
+          if (response == null) {
+              console.log("response was null");
+          } else {
+              console.log(response);
+          }
+      }).fail(function(error) {
+          console.log("post failed");
+          console.log(error);
+      })
+    }
+
+    $scope.exportDataToSpreadsheet = function (kottans) {
+      var csvData = getCsvData(kottans);
+      var result = formAjaxData(csvData);
+      var ajaxData = result[0], boundary = result[1];
+      if (!$scope.auth.thirdPartyUserData) {
+        return $scope.login().then(function(){
+          doPost(ajaxData, boundary)
+        })
+      }
+      return doPost(ajaxData, boundary)
+    };
 
     function loadKottans(){
       $scope.kottans = $firebase(kottansRef);
